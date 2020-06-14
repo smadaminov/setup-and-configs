a579331d6dc89caa937329d1ba80dab27d4972b2
Sergey Madaminov <smadaminov@cs.stonybrook.edu>
2020-06-14 02:09:28 -0400

    added more setup to the boostrap script: it will install the Hack font; it will install tmux 2.4 (so more features are avaiable as older ubuntu has older tmux and I don't know about later ubuntu) with is prereqs; put the simlink for the tmux conf

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,35 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish -y
 
+# Install patched nerd font Hack
+git clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install Hack
+cd .. && rm -rf nerd-fonts
+
+# Install tmux 2.4 version
+## First install required packages
+sudo apt-get install -y automake
+sudo apt-get install -y bison
+sudo apt-get install -y build-essential
+sudo apt-get install -y pkg-config
+sudo apt-get install -y libevent-dev
+sudo apt-get install -y libncurses5-dev
+sudo apt-get install -y xsel # This is required to support copy to OS clipboard
+## Install tmux
+rm -rf /tmp/tmux
+git clone https://github.com/tmux/tmux.git /tmp/tmux
+cd /tmp/tmux
+git checkout 2.4
+sh autogen.sh
+./configure && make
+sudo make install
+cd - && rm -rf /tmp/tmux
+
+# Tmux configuration files
+ln -sf $(pwd)/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf $(pwd)/dotfiles/.tmux.conf.local ~/.tmux.conf.local
+
 # Make sure that fish shell was installed
 if test -f "/usr/bin/fish"; then
     echo "fish shell has been successfully installed."
